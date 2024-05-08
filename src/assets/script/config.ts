d109741c5c7f701472066230cf8007fc36196c58
LMC <37393495+maotuworkroom@users.noreply.github.com>
2024-05-08 16:48:34 +0800

    Update config.ts

--- src/assets/script/config.ts
+++ src/assets/script/config.ts
@@ -1,6 +1,6 @@
 export const deploy = true;
 export let endpoint = "https://api.fystart.com";
 export let openai_endpoint = "wss://api.chatnio.net";
-export const qweather = "d25856c083574121a538e53952a2bfff";
+export const qweather = "3aeef1a01ee248f0bf913993fe3491b4";
 
 if (!deploy) endpoint = "http://localhost:8001";
